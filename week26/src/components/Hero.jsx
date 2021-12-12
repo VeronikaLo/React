1ca632259674b1d86d921baf5e1663cec66447ca
Veronika L <verokipri@yahoo.com>
2021-12-12 16:07:10 +0100

    +class syntax

--- week26/src/components/Hero.jsx
+++ week26/src/components/Hero.jsx
@@ -4,16 +4,17 @@ import './Hero.css';
 
 function Hero(props) {
 
+    const {heroName, universe, alterEgo, activity, friends, superPower, src} = props;
 
     return(
         <div className="card">
-         <div className="card__title">{props.heroName}</div>
-         <div className="card-item"><span>Вселенная: </span>{props.universe}</div>
-         <div className="card-item"><span>Альтер эго: </span>{props.alterEgo}</div>
-         <div className="card-item"><span>Род деятельности: </span>{props.activity}</div>
-         <div className="card-item"><span>Друзья: </span>{props.friends}</div>
-         <div className="card-item"><span>Суперсилы: </span>{props.superPower}</div>
-         <img className="picture" src={props.src} alt="" />
+         <div className="card__title">{ heroName }</div>
+         <div className="card-item"><span>Вселенная: </span>{ universe }</div>
+         <div className="card-item"><span>Альтер эго: </span>{ alterEgo }</div>
+         <div className="card-item"><span>Род деятельности: </span>{ activity }</div>
+         <div className="card-item"><span>Друзья: </span>{ friends }</div>
+         <div className="card-item"><span>Суперсилы: </span>{ superPower }</div>
+         <img className="picture" src={ src } alt="" />
 
 
         </div>
@@ -21,3 +22,35 @@ function Hero(props) {
 }
 
 export default Hero;
+
+
+/*
+// as a class component:
+
+import React from 'react';
+
+ class Hero extends React.Component{
+
+    render(){
+
+        const {heroName, universe, alterEgo, activity, friends, superPower, src} = this.props;
+
+        return(
+            <div className="card">
+             <div className="card__title">{ heroName }</div>
+             <div className="card-item"><span>Вселенная: </span>{ universe }</div>
+             <div className="card-item"><span>Альтер эго: </span>{ alterEgo }</div>
+             <div className="card-item"><span>Род деятельности: </span>{ activity }</div>
+             <div className="card-item"><span>Друзья: </span>{ friends }</div>
+             <div className="card-item"><span>Суперсилы: </span>{ superPower }</div>
+             <img className="picture" src={ src } alt="" />
+    
+    
+            </div>
+        );
+    }
+ }
+
+ export default Hero;
+
+ */
